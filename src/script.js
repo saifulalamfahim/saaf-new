de861a261a6d265c0d67719875b45bfaf8626462
Saiful Alam Fahim <fahim12345saiful@gmail.com>
2024-01-14 00:42:26 +0600

    experience section maybe complete

--- src/script.js
+++ src/script.js
@@ -74,4 +74,21 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".menu-close-btn").addEventListener("click", closeMenu);
     // document.querySelector(".menu-link").addEventListener("click", closeMenu);
     tl.reverse();
+    
   });
+
+
+
+// gsap.set(".position:not(:first-child)", {opacity:0, scale:0.5})
+
+// const animation = gsap.to(".position:not(:first-child)", {
+// 	opacity:1, scale:1, duration:1, stagger:1
+// })
+
+
+ScrollTrigger.create({
+	trigger:".gallery",
+	start:"top top",
+	end:"bottom bottom",
+	pin:".right-new"
+})
